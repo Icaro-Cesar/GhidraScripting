2d0a100db1e4e099bc918ef4307c543ea1641914
0x0d4y <icarocesar630@gmail.com>
2023-11-06 10:11:08 -0300

    Win32 API Extractor Update

--- Scripts/Win32API_Extrator.py
+++ Scripts/Win32API_Extrator.py
@@ -7,9 +7,10 @@ print("\nWin32 API Extrator\n")
 pe_stable = currentProgram.getSymbolTable()
 # Create the pe_exstable object, to acess the getExternalSymbols. Here we will collect the Imported Functions
 pe_exstable = pe_stable.getExternalSymbols()
+
 # Create a for loop, to print each Imported Function from the Import Table
 for apis in pe_exstable:
      api = str(apis)  # Convert the type of apis variable to string
-     print("Win32 API: "+api) # Print the results
-
-
+     print("------------------------------------------")
+     print("Win32 API Name: "+api) # Print the results
+     print("------------------------------------------")
