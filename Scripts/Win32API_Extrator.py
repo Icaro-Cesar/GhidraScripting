6a0796089fbf7e19496d3f041083e821b71bba95
Ícaro César <icaro.gomes@ish.com.br>
2023-11-06 21:55:32 -0300

    Atualização do Script Function/Win32API Extractor

--- Scripts/Win32API_Extrator.py
+++ Scripts/Win32API_Extrator.py
@@ -1,7 +1,7 @@
 #@author Ox0d4y
 #@category API Extrator
 
-print("\nWin32 API Extrator\n")
+print("\nExternal Function/Win32API Extrator\n")
 
 # Create the pe_stable object, to access the getSymbolTable on currentProgram
 pe_stable = currentProgram.getSymbolTable()
@@ -12,5 +12,5 @@ pe_exstable = pe_stable.getExternalSymbols()
 for apis in pe_exstable:
      api = str(apis)  # Convert the type of apis variable to string
      print("------------------------------------------")
-     print("Win32 API Name: "+api) # Print the results
+     print("External Function/Win32API Name: "+api) # Print the results
      print("------------------------------------------")
